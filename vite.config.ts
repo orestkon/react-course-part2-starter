4186e43ee22b8bd78b8690559bb4e0b8d131e07d
Orestis Konstantinidis <30441437+orestkon@users.noreply.github.com>
2025-12-05 21:58:12 +0200

    Fix a small vite configuration error.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // 🔑 Define a fallback value for the token
+  define: {
+    // Vite expects the value to be a JSON string, so use double quotes
+    __WS_TOKEN__: '""',
+  },
+});
